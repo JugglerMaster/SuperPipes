df7db8d1e5d4c2e43ac4cf1dcc0b358166de156f
JugglerMaster <JugglerMaster@users.noreply.github.com>
2017-07-27 13:58:35 -0500

    Update ModItems.java
    Add super iron

--- ModItems.java
+++ ModItems.java
@@ -4,11 +4,17 @@ import net.miecraft.item.Item;
 import net.minecraftforge.registries.IForgeRegistry;
 
 public class ModItems {
-
+  //my super metal
+  public static ItemBase ingotSuperIron = new ItemBase("ingot_superiron").setCreativeTab(CreativeTabs.MATERIALS);
+  
   public static void register(IForgeRegistry<Item> registry) {
+    registry.registerAll(
+      ingotSuperIron
+      );
   }
   
   public static void registerModels() {
+    ingotSuperIron.registerItemModel();
   }
 }
 
