f3db479803e31511b79990a96552bc8d2938165f
JugglerMaster <JugglerMaster@users.noreply.github.com>
2017-07-27 13:40:37 -0500

    Update ItemBase.java

--- ItemBase.java
+++ ItemBase.java
@@ -9,8 +9,18 @@ public class ItemBase extends Item {
 protected String name;
 
 public ItemBase(String name) {
-
+  this.name = name;
+  setUnlocalizedName(name);
+  setRegistryName(name);
 }
+public void registerItemModel() {
+  SuperPipes.proxy.registerItemRenderer(this, 0 , name);
+}
+ @Override
+  public ItemBase setCreativeTab(CreativeTabs tab) {
+    super.setCreativeTab(tab); 
+    return this;
+  }
 
 
 
