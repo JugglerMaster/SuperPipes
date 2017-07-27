2104936c4acc90a9bfa561b0cfc2b2431da5ba5d
JugglerMaster <JugglerMaster@users.noreply.github.com>
2017-07-27 13:37:41 -0500

    Update ClientProxy.java
    Added client side renderer code

--- ClientProxy.java
+++ ClientProxy.java
@@ -1,5 +1,9 @@
 package net.jugglermaster.superpipes;
 
 public static ClientProxy extends CommonProxy {
-
+  
+  @Override
+  public void registerItemRenderer(Item item, int meta, String id){
+   ModLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation(SuperPipes.modId + ":" + id, "inventory")); 
+  }
 }
