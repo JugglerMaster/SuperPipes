eafc2286cab221d8d76898206cf5f34dfa37ca89
JugglerMaster <JugglerMaster@users.noreply.github.com>
2017-07-27 13:14:30 -0500

    Create ItemBase.java
    Initial Commit

---
+++ ItemBase.java
@@ -0,0 +1,17 @@
+package net.jugglermaster.superpipes.item;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.jugglermaster.superpipes.SuperPipes;
+
+public class ItemBase extends Item {
+
+protected String name;
+
+public ItemBase(String name) {
+
+}
+
+
+
+}
