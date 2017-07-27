7415ed59d9cf7662720332bb7a2e665e970ddf4a
JugglerMaster <JugglerMaster@users.noreply.github.com>
2017-07-27 13:52:51 -0500

    Create ModItems.java
    Initial Commit

---
+++ ModItems.java
@@ -0,0 +1,14 @@
+package net.jugglermaster.superpipes.item;
+
+import net.miecraft.item.Item;
+import net.minecraftforge.registries.IForgeRegistry;
+
+public class ModItems {
+
+  public static void register(IForgeRegistry<Item> registry) {
+  }
+  
+  public static void registerModels() {
+  }
+}
+
