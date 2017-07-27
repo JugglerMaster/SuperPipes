7915bea5f287df4721f5df9bb2719eff42fb8780
JugglerMaster <JugglerMaster@users.noreply.github.com>
2017-07-27 15:59:18 -0500

    Update BlockBase.java
    Started adding block code

--- BlockBase.java
+++ BlockBase.java
@@ -1,1 +1,24 @@
+package net.jugglermaster.superpipes.block;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemBlock;
+import net.jugglermaster.superpipes.SuperPipes;
+
+public class BlockBase extends Block {
+  protected String name;
+  
+  public BlockBase(Material material, String name) {
+    super(material);
+    
+    this.name = name;
+    
+    setUnlocalizedName(name);
+    setRegistryName(name);
+  }
+  public void registerItemModel(ItemBlock itemblock) {
+    SuperPipes.proxy.registerItemRenderer(itemBlock, 0, name);
+  }
+  //stopped here
+}
