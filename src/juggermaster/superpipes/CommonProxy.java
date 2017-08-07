c0229b750349725606145ee60e5729e60d915043
JugglerMaster <JugglerMaster@users.noreply.github.com>
2017-08-07 16:33:44 -0500

    Gradlew setup
    I am trying to set it up correctly this time

--- src/juggermaster/superpipes/CommonProxy.java
+++
@@ -1,7 +0,0 @@
-
-package juggermaster.superpipes;
-
-public class CommonProxy {
-  public void registerItemRenderer(Item item, int meta, String id){ 
-  }
-}
--- src/juggermaster/superpipes/item/ItemModelProvider.java
+++
@@ -1,7 +0,0 @@
-package juggermaster.superpipes.item;
-
-import net.minecraft.item.Item;
-
-public interface ItemModelProvider {
-  void registerItemModel(Item item);
-}
